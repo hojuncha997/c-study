ade30a7f2f03dfee346bb12b0899dd2c74358bbe
Hojun Cha <hojun.cha997@gmail.com>
2023-09-24 22:38:32 +0900

    챕터2

---
+++ ch_02/2-8.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+int main(void)
+{
+    printf("학번 : %d\n", 32165);
+    printf("이름: %s\n", "차호준");
+    printf("학점: %.1lf\n", 4.4324);
+    printf("문자학점: %C\n", "A");
+
+    return 0;
+}
