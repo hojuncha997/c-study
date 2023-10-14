0e5b4a2b869de313ae895eafab3b7f566a8a08ef
Hojun Cha <hojun.cha997@gmail.com>
2023-10-14 16:19:53 +0900

    챕터 11-1

---
+++ ch_11-1(char)/11-q1.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+
+int main(void)
+{
+    char ch;
+
+    printf("문자 입력: ");
+
+    scanf("%c", &ch);
+
+    printf("%c 문자의 아스키 코드 값: %d\n", ch, ch);
+
+    return 0;
+}
+
+/*
+결과:
+문자 입력: Q
+Q 문자의 아스키 코드 값: 81
+*/
